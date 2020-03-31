f732efbd817cfd5b184ec2569c5f5a4628c2b29d
Hoang Dinh Minh <hoang.dm@seesaa.vn>
2020-03-31 15:18:36 +0700

    test

---
+++ Object Repository/Page_/html_(function(wdsli)wlwlwlpush(gtmstartnew_a09353.rs
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>html_(function(wdsli)wlwlwlpush(gtmstartnew_a09353</name>
+   <tag></tag>
+   <elementGuidId>aa19a1de-fb4d-4792-8517-ffe25f82842b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*/text()[normalize-space(.)='']/parent::*</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>html</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TGMB6TK');
+
+ログイン
+
+
+
+
+
+
+
+
+
+
+  var ua = navigator.userAgent;
+  if (ua.indexOf('iPad') > 0) {
+    document.write('&lt;meta name=&quot;viewport&quot; content=&quot;width=1280&quot;>');
+  } else {
+    document.write('&lt;meta name=&quot;viewport&quot; content=&quot;width=device-width,initial-scale=1&quot;>');
+  }
+
+//&lt;![CDATA[
+var jsVars = [];
+//]]>
+#katalon{font-family:monospace;font-size:13px;background-color:rgba(0,0,0,.7);position:fixed;top:0;left:0;right:0;display:block;z-index:999999999;line-height: normal} #katalon div{padding:0;margin:0;color:#fff;} #katalon kbd{display:inline-block;padding:3px 5px;font:13px Consolas,&quot;Liberation Mono&quot;,Menlo,Courier,monospace;line-height:10px;color:#555;vertical-align:middle;background-color:#fcfcfc;border:1px solid #ccc;border-bottom-color:#bbb;border-radius:3px;box-shadow:inset 0 -1px 0 #bbb;font-weight: bold} div#katalon-spy_elementInfoDiv {color: lightblue; padding: 0px 5px 5px} div#katalon-spy_instructionDiv {padding: 5px 5px 2.5px}
+
+
+
+&lt;iframe src=&quot;https://www.googletagmanager.com/ns.html?id=GTM-TGMB6TK&quot;
+height=&quot;0&quot; width=&quot;0&quot; style=&quot;display:none;visibility:hidden&quot;>&lt;/iframe>
+
+
+            
+                
+        
+                
+            E-mail address
+            
+                            
+                        Please enter your e-mail address.
+                    
+        
+            Password
+            
+                            
+                        
+                
+                Remember me
+            
+        
+
+                
+
+        
+            Login
+        
+                Forgot your password?
+    
+            
+    Copyright© Tribal Media House, Inc. All rights reserved.
+
+        
+    
+
+
+
+
+  $(document).ready(function() {
+    var footerId = &quot;footer&quot;;
+    var dh = document.getElementsByTagName(&quot;body&quot;)[0].clientHeight;
+    //�t�b�^�[��top����̈ʒu
+    document.getElementById(footerId).style.top = &quot;0px&quot;;
+    var ft = document.getElementById(footerId).offsetTop;
+    //�t�b�^�[�̍���
+    var fh = document.getElementById(footerId).offsetHeight;
+    //�E�B���h�E�̍���
+    if (window.innerHeight){
+      var wh = window.innerHeight;
+    }else if(document.documentElement &amp;&amp; document.documentElement.clientHeight != 0){
+      var wh = document.documentElement.clientHeight;
+    }
+    if(ft+fh&lt;wh){
+      document.getElementById(footerId).style.position = &quot;relative&quot;;
+      document.getElementById(footerId).style.top = 840 + &quot;px&quot;;
+      document.getElementById(footerId).style.display=&quot;block&quot;;
+    }
+  });
+
+
+
+/html[1]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//html</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/test/Page_/div_Please enter your e-mail address.rs
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Please enter your e-mail address</name>
+   <tag></tag>
+   <elementGuidId>1a479c5c-b752-44ba-90c3-1f97e1704229</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='UserLoginForm']/div[2]/div[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>ckt-login_content_box_error</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Please enter your e-mail address.</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;UserLoginForm&quot;)/div[@class=&quot;ckt-login_content_box&quot;]/div[@class=&quot;ckt-login_content_box_error&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='UserLoginForm']/div[2]/div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='E-mail address'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/preceding::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Remember me'])[1]/preceding::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Please enter your e-mail address.']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[3]</value>
+   </webElementXpaths>
+</WebElementEntity>
